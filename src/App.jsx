89276b2de6e216ae1e2d8b53f856e71308a2db9b
Gina Song <gina0song@gmail.com>
2025-12-10 14:20:40 -0800

    Update the ChatEntry and App components to display a single chat message bubble with the message body, timestamp, sender's name

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
         {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        Wave 02: Render ChatLog component */
+          <ChatEntry
+            sender="Vladimir"
+            body="Why are you arguing with me"
+            timeStamp="2018-05-29T22:49:06+00:00"
+          />
+        }
       </main>
     </div>
   );
