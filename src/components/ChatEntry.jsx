32f07838ae17460bac295aca50794f8c55cd6be9
Gina Song <gina0song@gmail.com>
2025-12-12 13:57:06 -0800

    Update ChatEntry.jsx to Handle Click Events, wave 3 completed

--- src/components/ChatEntry.jsx
+++ src/components/ChatEntry.jsx
@@ -2,7 +2,13 @@ import './ChatEntry.css';
 import PropTypes from 'prop-types';
 import TimeStamp from './TimeStamp';
 
-const ChatEntry = ({ sender, body, timeStamp }) => {
+const ChatEntry = ({id, sender, body, timeStamp, liked, onToggleLike }) => {
+
+  const handleLikeClick = () => {
+    onToggleLike(id);
+  };
+
+
   return (
     <section className="chat-entry local">
       <h2 className="entry-name">{sender}</h2>
@@ -11,15 +17,22 @@ const ChatEntry = ({ sender, body, timeStamp }) => {
         <p className="entry-time">
           <TimeStamp time={timeStamp} />
         </p>
-        <button className="like">🤍</button>
+        <button className="like" onClick={handleLikeClick}>
+          {liked ? '❤️' : '🤍'}
+
+        </button>
       </div>
     </section>
   );
 };
 
 ChatEntry.propTypes = {
+  id: PropTypes.number.isRequired,
   sender: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
   timeStamp: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  onToggleLike: PropTypes.func.isRequired,
 };
+
 export default ChatEntry;
